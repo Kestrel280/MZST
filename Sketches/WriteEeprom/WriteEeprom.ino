c1bf6934313d2b28383b6df73d430d5dbfacb659
Kestrel280 <downey.samp@gmail.com>
2024-09-23 17:38:25 -0400

    Moves Arduino sketches to a dedicated subfolder, to allow for a shared 'include' folder

---
+++ Sketches/WriteEeprom/WriteEeprom.ino
@@ -0,0 +1,11 @@
+#include <EEPROM.h>
+#include "../_include/NetworkModule.h"
+
+void setup() {
+  Serial.printf("");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Sketches/_include/NetworkModule.h
@@ -0,0 +1,9 @@
+#ifndef NETWORKMODULE_H
+#define NETWORKMODULE_H
+
+typedef struct _NetworkModule {
+	char networkSsid[32];
+	char networkPassword[32];
+} NetworkModule;
+
+#endif
