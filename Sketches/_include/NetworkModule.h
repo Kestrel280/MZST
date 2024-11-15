cea3f17d0a07b98ef5c6794989edb1f1026f8745
Kestrel280 <downey.samp@gmail.com>
2024-11-15 11:16:06 -0500

    Adds example _Networks.h file (secrets file for network info; git update-index --assume-unchanged _Networks.h)

--- Sketches/_include/NetworkModule.h
+++ Sketches/_include/NetworkModule.h
@@ -3,9 +3,12 @@
 
 #define EEPROM_SIZE 512
 
+#define NWM_MAXSSID_LEN 32
+#define NWM_MAXPW_LEN 32
+
 typedef struct _NetworkModule {
-	char networkSsid[32];
-	char networkPassword[32];
+	char networkSsid[NWM_MAXSSID_LEN];
+	char networkPassword[NWM_MAXPW_LEN];
 } NetworkModule;
 
 #endif
