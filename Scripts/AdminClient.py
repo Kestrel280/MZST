66a533f3cf549807c111920705b4b488883cb68b
Kestrel280 <downey.samp@gmail.com>
2025-05-12 13:38:43 -0400

    Improves AdminClient script: takes cli arguments + adds simple instruction comment

--- Scripts/AdminClient.py
+++ Scripts/AdminClient.py
@@ -1,13 +1,29 @@
 import socket
+import sys
+
+"""
+Utility script to send one-off commands to modules without requiring connection to server.
+Modules will listen for these "admin" while attemping to connect to the server.
+Example usage:
+
+python AdminClient.py 192.168.1.10,192.168.1.11,192.168.1.12 SET_EEPROM_VALUE SERVERIP 192.168.1.100
+"""
 
 ADMIN_PORT = 7777
 
-ips = input("Enter IPs, seperated by spaces, of modules to send to:\n").split(" ")
-command = input("Enter command to send to modules:\n")
+ips = sys.argv[1].split(',')
+command = ' '.join(sys.argv[2:])
+
+print(f"Attemping to issue command '{command}' to modules at following ips:")
+print(' | '.join(ips))
 
 for ip in ips:
-    print(f"Connecting to {ip}")
+    print(f"Connecting to {ip}... ", end='')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((ip, ADMIN_PORT))
-        s.sendall(str.encode(command))
-        print(f"Sent command to {ip}")
+        try:
+            s.settimeout(5)
+            s.connect((ip, ADMIN_PORT))
+            s.sendall(str.encode(command))
+            print(f"Success")
+        except TimeoutError:
+            print("FAILED due to timeout!")
