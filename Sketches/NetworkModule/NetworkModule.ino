733c1127b4d0ed35ae5d9a450f9169405ae2a66a
Kestrel280 <downey.samp@gmail.com>
2025-05-28 13:27:13 -0400

    Rips out rev2 RF/timing code for receiver, installs basic receiver code for rev3

--- Sketches/NetworkModule/NetworkModule.ino
+++ Sketches/NetworkModule/NetworkModule.ino
@@ -4,35 +4,41 @@
 #include "../_include/NetworkModule.h"
 #include "../_include/Server.h"
 #include "../_include/Eeprom_Helpers.h"
-#include "../_include/TimerSyncModule.h"
 #include "../_include/Utils.h"
 
 #include "State.h"
-#include "Timing.h"
-
-
 
 // Constants
 const int64_t UNTOUCH_TIMEOUT_US = 1000000;
 
 // Prototypes
 void touchpadCallback(void* param);
+void rfPacketCallback(void* param);
 void messageLoop(void* param);
 
 // Globals
 TaskHandle_t messageLoopTask;                       // Handle on message-loop thread
-TaskHandle_t rfListenerTask;                        // Handle on RF-listen-loop thread
+TaskHandle_t feedbackLoopTask;                      // Handle on feedback-loop thread
 State state;                                        // Current State of the node, updated via setState()
 StateData* stateData = nullptr;                     // State data associated with current state; always updated along with state
-int64_t dbg_time;                                   // ...
 int64_t timestampLastResetUs = 0;                   // Timestamp when the node received the most recent RF broadcast (in microseconds)
 int64_t timeLastTouchUs = 0;                        // Timestamp of last time node was touched (in microseconds)
 bool pendingServerUntouch = false;                  // Whether the server has sent us an UNTOUCH message that we haven't yet processed
 bool inTouchCooldown = false;                       // Whether we're in cooldown from the previous touch
+uint32_t touchIncrement, touchThreshold;            // Touchpad values
 
-// Program entrypoint
+/* ****************************************
+          Program entrypoint
+  Hardware setup
+  Connect to wifi
+  Connect to server/Receive admin-msgs
+  Register interrupts
+  Start thread fns/loops
+  ***************************************** */
 void setup() {
-  // Hardware setup
+  /* ************** */
+  /* Hardware Setup */
+  /* ************** */
   Serial.begin(SERIAL_BAUDRATE);
   EEPROM.begin(EEPROM_SIZE);
   pinMode(RF_D0, INPUT_PULLDOWN);
@@ -51,8 +57,10 @@ void setup() {
   ledcAttach(LED_BLUE_PIN, 5000, 8);
   dumpEeprom();
   readEeprom((char*)&module, 0, sizeof(module)); // Initialize module by loading from EEPROM
-
-  // Connect to wifi
+  
+  /* *************** */
+  /* Connect to wifi */
+  /* *************** */
   setState(INIT_WaitingWifi);
   WiFi.begin(module.networkSsid, module.networkPassword);
   while (WiFi.status() != WL_CONNECTED) {
@@ -60,10 +68,12 @@ void setup() {
     vTaskDelay(250);
   }
   Serial.printf("\nWiFi connected with IP: %s\n", WiFi.localIP().toString().c_str());
-
   sleep(2); // Pause a couple seconds... gives us time to restart/update the server if we just reset from a SHUTDOWN message
 
-  // Connect to server; while connecting, if we receive an admin connection, accept it and process whatever message it has for us
+  /* ***************** */
+  /* Connect to server */
+  /* ***************** */
+  // While connecting, if we receive an admin connection, accept it and process whatever message it has for us
   setState(INIT_WaitingServer);
   Serial.printf("Trying to connect to server socket at host %s:%d", module.serverIp, module.serverPort);
   adminSocketListener = WiFiServer(ADMIN_PORT);
@@ -82,44 +92,68 @@ void setup() {
       adminSocket.stop();
       Serial.printf("\nDisconnected from admin-socket, continuing to attempt connection to server\n");
     }
-
     vTaskDelay(250);
   }
   Serial.printf("\nConnected to server at host %s:%d\n", module.serverIp, module.serverPort);
   adminSocketListener.close();
-  Serial.printf("starting tasks\n");
+
+  /* ******************* */
+  /* Register interrupts */
+  /* ******************* */
+  // Touchpad interrupt
+  // ESP32 vs ESP32s3 differences: 
+  //    Touch values are normally typedef'd to touch_val_t, which is uint16_t on ESP32, uint32_t on ESP32s2/s3
+  //    touchPadInterrupt() behaves differently on ESP32 ('threshold' argument is a true threshold for ESP32, but on ESP32s2/s3 it's an INCREMENT value)
+  // Using uint32_t explicitly, caused some headaches in the past
+  uint32_t _touchVal;
+  _touchVal = touchRead(TP_PIN);
+  touchIncrement = _touchVal / 8;
+  touchThreshold = _touchVal + touchIncrement;
+  sendMessage(createOutboundMessage(MTYPE_REGISTER_NODE, touchThreshold));
+  touchAttachInterrupt(TP_PIN, &touchpadCallback, touchIncrement);
+  Serial.printf("Baseline tp val = %d; set threshold to %d\n", _touchVal, touchThreshold);
+
+  // RF packet interrupt
+  attachInterrupt(RF_VT, rfPacketCallback, RISING);
+
+  /* ****************** */
+  /* Start master loops */
+  /* ****************** */
+  Serial.printf("Setup complete, starting tasks\n");
   setState(INIT_Complete);
 
   xTaskCreatePinnedToCore(        // Message loop on core 0
-    messageLoop,      /* Task function. */
-    "Message_Loop",   /* name of task. */
-    16384,            /* Stack size of task */
-    NULL,             /* parameter of the task */
-    0,                /* priority of the task */
-    &messageLoopTask, /* Task handle to keep track of created task */
-    0);               /* pin task to core 0 */
-  Serial.printf("started message loop\n");
+    messageLoop,        /* Task function. */
+    "Message_Loop",     /* name of task. */
+    16384,              /* Stack size of task */
+    NULL,               /* parameter of the task */
+    0,                  /* priority of the task */
+    &messageLoopTask,   /* Task handle to keep track of created task */
+    0);                 /* pin task to core 0 */
 
-  xTaskCreatePinnedToCore(        // RF receiver loop on core 1
-    rfListen,         /* Task function. */
-    "RF_Listen",      /* name of task. */
-    16384,            /* Stack size of task */
-    NULL,             /* parameter of the task */
-    0,                /* priority of the task */
-    &rfListenerTask,  /* Task handle to keep track of created task */
-    1);               /* pin task to core 1 */
-  Serial.printf("started rf receiver\n");
-  sleep(1);
+  xTaskCreatePinnedToCore(        // Feedback loop on core 1
+    feedbackLoop,       /* Task function. */
+    "Feedback_Loop",    /* name of task. */
+    16384,              /* Stack size of task */
+    NULL,               /* parameter of the task */
+    0,                  /* priority of the task */
+    &feedbackLoopTask,  /* Task handle to keep track of created task */
+    1);                 /* pin task to core 1 */
   return;
 }
 
+// loop() is automatically entered after setup() returns
+// We're managing our own loops, so just kill this thread
 void loop() {
-  vTaskDelete(NULL); // Kill this thread; loop() should be dead
+  vTaskDelete(NULL);
   /* unreachable */
   Serial.printf("---Unreachable code in loop()---");
   return;
 }
 
+/* ****************************************
+          ISRs
+  ***************************************** */
 // ISR for when touchpad is triggered
 void touchpadCallback() {
   if (!inTouchCooldown) {
@@ -133,22 +167,27 @@ void touchpadCallback() {
   return;
 }
 
-// Master message loop. Intended to run in its own thread
+// ISR for when RF_VT goes high (RF packet received)
+void rfPacketCallback() {
+  char data = 0b0;
+  data = \
+    digitalRead(RF_D0) << 7 |\
+    digitalRead(RF_D1) << 6 |\
+    digitalRead(RF_D2) << 5 |\
+    digitalRead(RF_D3) << 4 |\
+    digitalRead(RF_D4) << 3 |\
+    digitalRead(RF_D5) << 2 |\
+    digitalRead(RF_D6) << 1 |\
+    digitalRead(RF_D7);
+  Serial.printf("RF received: %x\n", data);
+}
+
+/* ****************************************
+          Loops/Thread fns
+  ***************************************** */
+// Master message loop
 void messageLoop(void* param) {
   Serial.printf("Message loop running on core %d\n", xPortGetCoreID());
-  
-  // Normally aliased to touch_val_t
-  //   which is uint16_t on ESP32, uint32_t on ESP32s2/s3
-  // Explicitly use uint32_t because the behavior of touchPadInterrupt() is different for both anyway
-  // ('threshold' argument is a true threshold for ESP32, but on ESP32s2/s3 it's an INCREMENT value)
-  uint32_t _touchVal, touchIncrement, touchThreshold;
-  _touchVal = touchRead(TP_PIN);
-  touchIncrement = _touchVal / 8;
-  touchThreshold = _touchVal + touchIncrement;
-  sendMessage(createOutboundMessage(MTYPE_REGISTER_NODE, touchThreshold));
-  touchAttachInterrupt(TP_PIN, &touchpadCallback, touchIncrement);
-  Serial.printf("Baseline tp val = %d; set threshold to %d\n", _touchVal, touchThreshold);
-
   while (true) {
     // Send messages
     while(!outboundMessageQueue.empty()) {
@@ -164,6 +203,15 @@ void messageLoop(void* param) {
       processIncomingMessage(line);
     }
 
+  }
+  /* unreachable */
+  return;
+}
+
+// Master feedback-submodule loop
+void feedbackLoop(void* param) {
+  Serial.printf("Feedback loop running on core %d\n", xPortGetCoreID());
+  while (true) {
     // Reset to untouch color iff server has sent us UNTOUCH and we're ready
     //  We're ready if UNTOUCH_TIMEOUT has passed since last touch and we're not currently touching
     if (inTouchCooldown) {
@@ -176,11 +224,13 @@ void messageLoop(void* param) {
       pendingServerUntouch = false;
       writeLed(stateData->colorIdle);
     }
+  vTaskDelay(250);
   }
-  /* unreachable */
-  return;
 }
 
+/* ****************************************
+          Misc
+  ***************************************** */
 void processModuleSpecificMessage(std::string msg) {
   std::stringstream iss(msg);
   std::string command, value;
--- Sketches/NetworkModule/Timing.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIMING_H
-#define TIMING_H
-
-int countSetBits(int inp);
-int countSetBitsFixedTime(int inp);
-void rfListen(void* param);
-
-#endif
