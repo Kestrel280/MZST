c7848e7a6515f8bcd343414dbfbf2c1617fc3930
Kestrel280 <downey.samp@gmail.com>
2025-06-10 15:16:52 -0400

    Adds comments to NVSUtil header

--- Firmware/Module/components/NVSUtil/include/NVSUtil.h
+++ Firmware/Module/components/NVSUtil/include/NVSUtil.h
@@ -1,10 +1,10 @@
 #include "esp_system.h"
 
-void nvsInit();
-bool nvsGetInt(const char* key, uint32_t* out);
-bool nvsGetStr(const char* key, char** pOut);
-bool nvsSetInt(const char* key, uint32_t val);
-bool nvsSetStr(const char* key, const char* val);
-bool nvsCommit();
-void nvsDump();
-void nvsWipe();
+void nvsInit();                                     // Initial NVS setup -- required before calling any other NVSUtil function
+bool nvsGetInt(const char* key, uint32_t* out);     // Retrieve int32 value from NVS, stores it in 'out'. Returns 'false' on error (incl. key not found)
+bool nvsGetStr(const char* key, char** pOut);       // Retrieve string from NVS, stores it in heap and places pointer in 'pOut'. Returns 'false' if key not found
+bool nvsSetInt(const char* key, uint32_t val);      // Set int32 value on NVS. Returns 'false' on error. Must call nvsCommit() afterwards (recommended to batch nvsSet...() calls before nvsCommit())
+bool nvsSetStr(const char* key, const char* val);   // Set string on NVS. Returns 'false' on error. Must call nvsCommit() afterwards (recommended to batch nvsSet...() calls before nvsCommit())
+bool nvsCommit();                                   // Commits any changes specified by nvsSet...() functions
+void nvsDump();                                     // (unimplemented) Prints all key:value pairs on NVS
+void nvsWipe();                                     // (unimplemented) Wipes all NVS data
